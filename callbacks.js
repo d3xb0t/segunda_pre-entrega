8f8482a9827cd51b60e99723e7804aa2ea5977de
d3xb0t <d3xb0t@gmail.com>
2023-03-30 12:44:59 -0500

    Agregando metodos auxiliares a archivo externo

---
+++ callbacks.js
@@ -0,0 +1,23 @@
+let findProduct = function (id, products){
+    return products.find((product) => product.id === id)
+  }
+
+let verifyUniqueness = function (code, products) {
+    if (products.find((product) => product.code === code)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+let verifyObjectParameters = function (array, producto) {
+  let flags = []
+  flags = array.map((parametro) => Object.keys(producto).some(x => x == parametro))
+  return flags.includes(false)
+}
+
+module.exports = {
+    findProduct,
+    verifyUniqueness,
+    verifyObjectParameters
+}
--- productManager.js
+++ productManager.js
@@ -1,5 +1,7 @@
 const fs = require('fs')
+const callback = require('./callbacks')
 const Product = require('./product.js')
+
 
 class ProductManager {
   id = 0;
@@ -9,32 +11,14 @@ class ProductManager {
     this.#path = path?path:this.#path
   }
 
-  findProduct(id, products){
-    return products.find((product) => product.id === id)
-  }
-
-  verifyUniqueness(code, products) {
-    if (products.find((product) => product.code === code)) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  verifyObjectParameters(array, producto) {
-    let flags = []
-    flags = array.map((parametro) => Object.keys(producto).some(x => x == parametro))
-    return flags.includes(false)
-}
-
   async addProduct(producto) {
     const array = ['title', 'description', 'price', 'thumbnail', 'code', 'stock']
-    if(this.verifyObjectParameters(array, producto)){
+    if(callback.verifyObjectParameters(array, producto)){
       console.log("Los parametros deben estar completos")
     }else{
         let resultado = await fs.promises.readFile(this.#path, 'utf-8')
         let products = JSON.parse(resultado)
-        if (!this.verifyUniqueness(producto.code, products)) {
+        if (!callback.verifyUniqueness(producto.code, products)) {
           this.id++;
           const item = new Product(
             this.id,
@@ -61,7 +45,7 @@ class ProductManager {
     let item = products.find((product) => product.id === id)
     if (item) {
       products = products.filter((product) => product.id !== id)
-      if(!(this.findProduct(id, products))){
+      if(!(callback.findProduct(id, products))){
         await fs.promises.writeFile(this.#path, JSON.stringify(products)) 
         console.log(`the product with id ${id} has been removed`) 
       }  
@@ -76,7 +60,7 @@ class ProductManager {
     }
     let resultado = await fs.promises.readFile(this.#path, 'utf-8')
     let products = JSON.parse(resultado)
-    let item = this.findProduct(id, products)
+    let item = callback.findProduct(id, products)
     if (item) {
       try{
         Object.keys(modificador).map((propiedad) => item[propiedad] = modificador[propiedad])  
@@ -97,7 +81,7 @@ class ProductManager {
   async getProductByid(id) {
     let resultado = await fs.promises.readFile(this.#path, 'utf-8')
     let products = JSON.parse(resultado)
-    let item = this.findProduct(id, products)
+    let item = callback.findProduct(id, products)
     if (item) {
       console.log(item);
     } else {
