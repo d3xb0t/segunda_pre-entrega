4c5d71875f9a34980c20b618e478460b492890d6
d3xb0t <d3xb0t@gmail.com>
2023-04-27 15:51:14 -0500

    Se agrega archivos index.html, style.css y main.js para obtener el array de productos sin necesidad de handlebars a modo de prueba

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const productRouter = require('./routes/productsRoutes')
 const cartRouter = require('./routes/cartsRoutes')
+const cors = require('cors')
 const express = require('express')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 app.use('/api/products', productRouter)
