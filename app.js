40a79b7f91b0d880dfbf4aeaa20fd8068eb99a50
d3xb0t <d3xb0t@gmail.com>
2023-05-22 06:48:56 -0500

    habilitado /api/products, /api/carts

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-const productos = require('./products.json')
+//const productos = require('./products.json')
 const express = require('express')
-const { Server } = require('socket.io')
+//const { Server } = require('socket.io')
 const app = express()
-const handlebars = require('express-handlebars')
+//const handlebars = require('express-handlebars')
 const path = require('path')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+/*
 app.engine('handlebars', handlebars.engine({ 
     defaultLayout: 'main',
     layoutsDir: path.join(__dirname, 'views/layouts')
 }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
-
+*/
 
 const productRouter = require('./routes/productsRoutes')
 const cartRouter = require('./routes/cartsRoutes')
+/*
 const homeRouter = require('./routes/homeRoutes')
 const realTimeProducts = require('./routes/realTimeProductsRoutes')
 const cors = require('cors')
-
-
 app.use(cors())
-
+*/
 app.use((requests, response, next) => {
     if(requests.body && requests.body._method){
         requests.method = requests.body._method
@@ -33,11 +33,14 @@ app.use((requests, response, next) => {
     next()
 })
 
-app.use('/realtimeproducts', realTimeProducts)
-app.use('/', homeRouter)
+//app.use('/realtimeproducts', realTimeProducts)
+//app.use('/', homeRouter)
 app.use('/api/products', productRouter)
 app.use('/api/carts', cartRouter)
 
+app.listen(8080, () => {console.log("Listening in port 8080")})
+
+/*
 const server = app.listen(8080, () => {console.log("Listen in port 8080")})
 const io = new Server(server)
 
@@ -47,3 +50,4 @@ io.on('connection', socket => {
         io.emit('log', data)
     })
 })
+*/
