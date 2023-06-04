38fed81492a857e6e332d02acb5703fbd2b742e6
d3xb0t <d3xb0t@gmail.com>
2023-06-03 19:42:53 -0500

    Agregada capacidad al carrito que le permite eliminar todos sus productos

--- routes/cartsRoutes.js
+++ routes/cartsRoutes.js
@@ -68,13 +68,29 @@ cartRouter.delete('/:cid/product/:pid', async(requests, response) => {
         response.send({status: "Impossible task", payload: error})
     }    
 })
+
+cartRouter.delete('/:cid', async (requests, response) => {
+    let { cid } = requests.params
+    try {
+        let carrito = await cartModel.findOne({_id: cid})
+        while(carrito.products.length > 0) {
+            carrito.products.pop()
+            carrito = await carrito.save()
+        }
+        response.status(201).send(carrito)
+    } catch (error) {
+        console.log("Imposible conectarse a la base de datos o id inexistente")
+        response.send({status: "Impossible task", payload: error}) 
+    }
+    
+})
 /*
 
     Además, agregar al router de carts los siguientes endpoints:
-    DELETE api/carts/:cid/products/:pid deberá eliminar del carrito el producto seleccionado.
+    DELETE api/carts/:cid/products/:pid deberá eliminar del carrito el producto seleccionado. OK
     PUT api/carts/:cid deberá actualizar el carrito con un arreglo de productos con el formato especificado arriba.
     PUT api/carts/:cid/products/:pid deberá poder actualizar SÓLO la cantidad de ejemplares del producto por cualquier cantidad pasada desde req.body
-    DELETE api/carts/:cid deberá eliminar todos los productos del carrito 
+    DELETE api/carts/:cid deberá eliminar todos los productos del carrito OK
     Esta vez, para el modelo de Carts, en su propiedad products, el id de cada producto generado dentro del array tiene que hacer referencia al modelo de Products. Modificar la ruta /:cid para que al traer todos los productos, los traiga completos mediante un “populate”. De esta manera almacenamos sólo el Id, pero al solicitarlo podemos desglosar los productos asociados.
 
 
